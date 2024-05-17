ae48993c5241c6ba697e553df5988803093de5aa
Iago Borelli <iago.borelli1304@gmail.com>
2024-05-17 18:41:18 -0300

    update all page

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,17 @@ window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
+
+
+
+const dataAtual = new Date();
+const anoAtual = dataAtual.getFullYear();
+const elementoAno = document.getElementById('anoAtual');
+elementoAno.textContent = anoAtual;
+
+
+const toggleButton = document.getElementById('toggle-dark-mode');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
